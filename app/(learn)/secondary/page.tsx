01a918cc71d11ab95a99927723a2b8cbd7829c99
sovor <sovoreric@gmail.com>
2025-06-01 15:16:00 +0000

    3 commit

--- app/(learn)/secondary/page.tsx
+++ app/(learn)/secondary/page.tsx
@@ -1,10 +1,46 @@
+import Link from "next/link";
+
+import styles from "../learn.module.css"
 
 
 export default function Secondary() {
     return (
-        <div className="container">
-            <h1>Secondary Levels</h1>
+
+
+        <div>
+
+            <section>
+                <div className="container">
+                    <h1>Senior High Levels</h1>
+                    <p>SHS 1 to SHS 3</p>
+                </div>
+            </section>
+
+
+            <section className="container section-box">
+
+                <div className="grid grid-cols-2 gap-4">
+
+                        <Link href="#" className={styles.cusLink}>
+                            Senior High
+                        </Link>
+
+                        <Link  href="#" className={styles.cusLink}>
+                            STEM
+                        </Link>
+
+
+
+
+
+                </div>
+
+            </section>
+
+
+
 
         </div>
+
     );
 }
